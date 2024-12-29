9e3bd3762d6f443fc1654d91c739d4056fd8b31e
JH <36171973+jahidhasanlinix@users.noreply.github.com>
2024-12-29 17:53:46 -0600

    Add files via upload

---
+++ Project_Factory/source_destination_query_data.sql
@@ -0,0 +1,18 @@
+CREATE TABLE data_sources_sales_csv
+(
+Branch_ID Varchar(200),
+Dealer_ID Varchar(200),
+Model_ID Varchar(200),
+Revenue BIGINT,
+Units_Sold BIGINT,
+Date_ID Varchar(200),
+Day INT,
+Month INT,
+Year INT,
+BranchName Varchar(1000),
+DealerName Varchar(1000),
+Product_Name Varchar(1000)
+)
+
+
+select * from data_sources_sales_csv;
